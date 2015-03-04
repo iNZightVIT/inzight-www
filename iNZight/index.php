b30c07a6df2aad947feac0edd9c33588a91ac4eb
tell029 <tell029@aucklanduni.ac.nz>
2015-03-04 22:47:03 +1300

    first commit

---
+++ iNZight/index.php
@@ -0,0 +1,45 @@
+<?php
+  $inzight_text = "<span class='col1'>i</span><span class='col2'>nz</span><span class='col1'>ight</span>";
+?>
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <title>iNZight for Data Analysis</title>
+  </head>
+
+  <body>
+    <div class="wrapper">
+      <div class="header">
+        <div class="topnav">
+          <div class="logo">
+            <img src="img/inzight_transp.png">
+          </div>
+
+
+        </div>
+      </div>
+      <div class="vspace"></div>
+
+
+      <div class="blurb">
+        <?php echo $inzight_text; ?> is a simple data analysis system which was initially designed for high school students to help explore data fast and easy without having to learn complex statistical software. By popular demand, it has been extended to handle multi-variable graphics, time series analysis and generalised linear models.
+      </div>
+
+
+
+      <div class="push"></div>
+    </div>
+
+    <div class="footer">
+      <div class="footer-content">
+        Footer goes here ...
+      </div>
+    </div>
+  </body>
+</html>
