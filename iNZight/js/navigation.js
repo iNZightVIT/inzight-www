aaea1f4db6371243bd1ba9e765459eb461ad32cb
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-04-28 15:48:09 +1200

    fix nav click bug

--- iNZight/js/navigation.js
+++ iNZight/js/navigation.js
@@ -4,8 +4,21 @@ $(".navstack").click(function() {
 });
 
 $(".topnav>ul>li").click(function(){
-  $(this).children('ul').addClass('current');
-  $(".topnav ul>li>ul").not('.current').hide();
-  $(this).children('ul').toggle();
-  $("ul.current").removeClass("current");
+  if ($(".navstack").is(":visible")) {
+    $(this).children('ul').addClass('current');
+    $(".topnav ul>li>ul").not('.current').hide();
+    $(this).children('ul').toggle();
+    $("ul.current").removeClass("current");
+  }
+});
+
+/*********************************************************************
+A problem when increasing screen size (perhaps rotating a tablet)
+is that any clicked links become active and drop-downs don't work.
+The below listener resets the display when the screen is made bigger.
+ *********************************************************************/
+$(window).resize(function() {
+  if ($(".navstack").not(":visible")) {
+    $(".topnav>ul").children("li").children('ul').css("display", "");
+  }
 });
