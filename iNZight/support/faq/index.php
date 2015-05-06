eeabd47553c107ef03332468f79e68539b58353f
tell029 <tell029@aucklanduni.ac.nz>
2015-05-07 09:55:55 +1200

    trying some things with FAQ

--- iNZight/support/faq/index.php
+++ iNZight/support/faq/index.php
@@ -2,14 +2,25 @@
 $rel = "../../";
 require_once($rel . 'assets/includes/1-top_matter.php');
 require_once($rel . 'assets/includes/2-header.php');
+require_once($rel . 'assets/libraries/md.php');
+
+$Pd = new ParsedownExtra();
+
+$faq = json_decode(file_get_contents("faq.json"));
+
+$faq = file_get_contents("faq.Md");
 
 ?>
 
 <h3>iNZight FAQ (Frequently Asked Questions)</h3>
 
-<p>
-  Stuff...
-</p>
+<div class="markdown faq">
+  <?php
+    //$first = $faq->download->Windows->Question;
+    //echo $Pd->text($first);
+    echo $Pd->text($faq);
+  ?>
+</div>
 
 
 
