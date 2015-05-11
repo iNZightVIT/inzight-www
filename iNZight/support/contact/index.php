9aeb2fb6ffa2a32f79c908c5518d8793fc42fefd
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-05-12 11:40:33 +1200

    redirect contact form

--- iNZight/support/contact/index.php
+++ iNZight/support/contact/index.php
@@ -1,4 +1,7 @@
 <?php
+
+header("Location: https://www.stat.auckland.ac.nz/~wild/iNZight/report.html");
+
 $rel = "../../";
 require_once($rel . 'assets/includes/1-top_matter.php');
 require_once($rel . 'assets/includes/2-header.php');
@@ -8,7 +11,8 @@ require_once($rel . 'assets/includes/2-header.php');
 <h3>Contact iNZight Support</h3>
 
 <p>
-  From and stuff.
+  For now, use the old website's contact form:
+  <a href="https://www.stat.auckland.ac.nz/~wild/iNZight/report.html">click here</a>.
 </p>
 
 
