555f40ce153db85c0f2f0fe46e9cc5b5839572c6
Tom Elliott <tom.elliott@auckland.ac.nz>
2015-10-29 10:42:45 +1300

    better FAQ navigation; added/fixed a few entries

--- iNZight/support/faq/index.php
+++ iNZight/support/faq/index.php
@@ -75,8 +75,6 @@ if (isset($_GET["section"])) {
 
 
 <?php
-
-
   if ($display) { ?>
     <div class="contents_list">
       <!-- <div class="label">Table of Contents</div> -->
@@ -101,43 +99,69 @@ if (isset($_GET["section"])) {
         ?>
       </ol>
     </div>
-  <?php } else { ?>
+  <?php } else {
+    $file = $file . ".Md";
+    if (file_exists($file)) {
+      // display the contents
+      include_once($rel . 'assets/libraries/md.php');
+      $Pd = new ParsedownExtra();
+      $text = file_get_contents($file);
+
+    ?>
+
+    <div class="contents_list">
+      <ul>
+        <?php
+          $topicSearch = '#';
+          $topicPattern = preg_quote($topicSearch, '/');
+          $pattern = "/^$topicPattern.*\$/m";
+          if(preg_match_all($pattern, $text, $matches)){
+            foreach($matches[0] as $topic) {
+              $IDpattern = "/\{#.*\}/";
+              if (preg_match($IDpattern, $topic, $this_id)) {
+                $href = substr($this_id[0], 1, strlen($this_id[0])-2);
+              } else {
+                $href = "#";
+              }
+
+              $li_text = substr(explode("{", $topic)[0], 1);
+              echo "<li><a href='$href'>$li_text</a></li>";
+            }
+          }
+          else{
+             echo "No matches found";
+          }
+        ?>
+      </ul>
+    </div>
 
     <div class="markdown faq">
 
     <?php
-      $file = $file . ".Md";
-      if (file_exists($file)) {
-        // display the contents
-        include_once($rel . 'assets/libraries/md.php');
-        $Pd = new ParsedownExtra();
-        $text = file_get_contents($file);
+      // search for Videos:
+      $textArray = explode("///", $text);
 
-        // search for Videos:
-        $textArray = explode("///", $text);
-
-        foreach($textArray as $text) {
-          if (preg_match("/^VIDEO: /", $text)) {
-            // remove the video text and ponk the URL down:
-            echo "<div class='video-wrapper asp16x9 halfsize'>";
-            echo "  <iframe width='560' height='315'";
-            echo "   src='".str_replace("VIDEO: ", "", $text)."'";
-            echo "   frameborder='0' allowfullscreen></iframe>";
-            echo "</div>";
-          } else if (preg_match("/^SCRIPT: /", $text)) {
-            echo "<script src='". str_replace("SCRIPT: ", "", $text) ."'></script>";
-          } else {
-            echo $Pd->text($text);
-          }
+      foreach($textArray as $text) {
+        if (preg_match("/^VIDEO: /", $text)) {
+          // remove the video text and ponk the URL down:
+          echo "<div class='video-wrapper asp16x9 halfsize'>";
+          echo "  <iframe width='560' height='315'";
+          echo "   src='".str_replace("VIDEO: ", "", $text)."'";
+          echo "   frameborder='0' allowfullscreen></iframe>";
+          echo "</div>";
+        } else if (preg_match("/^SCRIPT: /", $text)) {
+          echo "<script src='". str_replace("SCRIPT: ", "", $text) ."'></script>";
+        } else {
+          echo $Pd->text($text);
         }
-      } else {
-        echo "Something went wrong ... " . $file;
       }
-    ?>
+    } else {
+      echo "Something went wrong ... " . $file;
+    }
+  ?>
 
-    </div>
+  </div>
   <?php } ?>
-
 
 <?php
 require_once($rel . 'assets/includes/3-bottom_matter.php');
