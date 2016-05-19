88fc86cf885d244ff530ad1d92a6a7038140c603
Tom Elliott <tom.elliott@auckland.ac.nz>
2016-05-19 15:58:45 +1200

    add beta page

---
+++ iNZight/beta/index.php
@@ -0,0 +1,15 @@
+<?php
+$rel = "../";
+require_once($rel . 'assets/includes/1-top_matter.php');
+require_once($rel . 'assets/includes/2-header.php');
+?>
+
+<div class="container">
+  <h2>iNZight Beta</h2>
+
+</div>
+
+
+<?php
+require_once($rel.'assets/includes/3-bottom_matter.php');
+?>
