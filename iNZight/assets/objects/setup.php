d89e41fb9ded618272d03e0ba57e2a0af0939059
Tom Elliott <tomelliottnz@gmail.com>
2019-09-02 09:52:14 +1200

    Version bump

--- iNZight/assets/objects/setup.php
+++ iNZight/assets/objects/setup.php
@@ -25,8 +25,8 @@
     "linux" => "https://raw.githubusercontent.com/iNZightVIT/dev/master/scripts/install_inzight.sh"
   );
 
-  $inzight_version = "3.4.4";
-  $release_date    = "26 August 2019";
+  $inzight_version = "3.4.5";
+  $release_date    = "2 September 2019";
 
   $inzight_online_link = "http://lite.docker.stat.auckland.ac.nz/";
   $inzight_repository_link = "http://r.docker.stat.auckland.ac.nz";
