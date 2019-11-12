7219f140c87e657c533277aec8a715dee4b88fd2
Tom Elliott <tomelliottnz@gmail.com>
2019-11-12 16:19:24 +1300

    Update 3.5, + changelogs

--- iNZight/assets/objects/setup.php
+++ iNZight/assets/objects/setup.php
@@ -25,8 +25,8 @@
     "linux" => "https://raw.githubusercontent.com/iNZightVIT/dev/master/scripts/install_inzight.sh"
   );
 
-  $inzight_version = "3.4.6";
-  $release_date    = "23 September 2019";
+  $inzight_version = "3.5";
+  $release_date    = "11 November 2019";
 
   $inzight_online_link = "http://lite.docker.stat.auckland.ac.nz/";
   $inzight_repository_link = "http://r.docker.stat.auckland.ac.nz";
