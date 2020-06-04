ddf2f04c4a916336aeaaec40a0b1c784e5cb082c
Tom Elliott <tomelliottnz@gmail.com>
2020-06-05 10:34:14 +1200

    Use magic_index for data_options

--- iNZight/user_guides/data_options/contents.js
+++ iNZight/user_guides/data_options/contents.js
@@ -2,5 +2,9 @@
 "index" : {
   "title" : "Dataset Menu Operations",
   "desc"  : "Sometimes you will need to reorgnise your data set, either by filtering out unwanted rows, or combining columns. This section goes over the options available in iNZight."
+},
+"data_options" : {
+  "title" : "Dataset Menu Operations",
+  "desc"  : "Sometimes you will need to reorgnise your data set, either by filtering out unwanted rows, or combining columns. This section goes over the options available in iNZight."
 }
 }
--- iNZight/user_guides/data_options/index.php
+++ iNZight/user_guides/data_options/index.php
@@ -1,45 +1,9 @@
 <?php
 
 $rel = "../../";
-$crumbs = array("User Guides" => "../", "Dataset Menu" => "active");
-require_once($rel . 'assets/includes/1-top_matter.php');
-require_once($rel . 'assets/includes/2-header.php');
+$baseCrumb = "User Guides";
 
-$cont = json_decode(file_get_contents("contents.js"));
+$contents = json_decode(file_get_contents("contents.js"));
+$topic = "data_options";
 
-?>
-<div class="container">
-
-  <div class="markdown">
-    <?php
-      // display the contents
-      include_once($rel . 'assets/libraries/md.php');
-      $Pd = new ParsedownExtra();
-      $text = file_get_contents("data_options.Md");
-
-      // search for Videos:
-      $textArray = explode("///", $text);
-
-      foreach($textArray as $text) {
-        if (preg_match("/^VIDEO: /", $text)) {
-          // remove the video text and ponk the URL down:
-          echo "<div class='video-wrapper asp16x9'>";
-          echo "  <iframe width='560' height='315'";
-          echo "   src='".str_replace("VIDEO: ", "", $text)."'";
-          echo "   frameborder='0' allowfullscreen></iframe>";
-          echo "</div>";
-        } else {
-          echo $Pd->text($text);
-        }
-      }
-    ?>
-  </div>
-
-
-</div>
-
-
-
-<?php
-require_once($rel . 'assets/includes/3-bottom_matter.php');
-?>
+require_once($rel . 'assets/includes/magic_index.php');
