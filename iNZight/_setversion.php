48fd0c31ee31e00c209e12f0f716f9590456483b
Tom Elliott <tomelliottnz@gmail.com>
2020-06-05 10:19:27 +1200

    Fix PHP SESSION save location

--- iNZight/_setversion.php
+++ iNZight/_setversion.php
@@ -1,6 +1,10 @@
 <?php
+
+  session_save_path("/tmp");
   session_start();
   if (isset($_POST["version"])) {
     $_SESSION["VLITE"] = $_POST["version"] == "lite";
   }
+
+  var_dump($_SESSION);
 ?>
--- iNZight/hideMessage.php
+++ iNZight/hideMessage.php
@@ -1,3 +1,4 @@
 <?php
+session_save_path("/tmp");
 session_start();
 $_SESSION["hide"] = true;
