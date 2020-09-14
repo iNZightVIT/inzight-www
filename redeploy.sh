53a1c6fd0b475fe62af1c66cdcf22c091361d3b8
Tom Elliott <tomelliottnz@gmail.com>
2020-09-14 16:16:31 +1200

    Typo

--- redeploy.sh
+++ redeploy.sh
@@ -7,7 +7,7 @@ TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 /opt/bitnami/git/bin/git checkout $TAG
 
 # any changes to webhook.service
-cp inzight-www/webhook.service /lib/systemd/system/
+cp webhook.service /lib/systemd/system/
 cp /lib/systemd/system/webhook.service /etc/systemd/system/webhook.service
 chmod 644 /etc/systemd/system/webhook.service
 systemctl daemon-reload
