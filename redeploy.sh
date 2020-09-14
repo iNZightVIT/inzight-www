d98175c3d9e9b2955ffbc24fea96bb20f6358c73
Tom Elliott <tomelliottnz@gmail.com>
2020-09-14 16:10:02 +1200

    Define tag after fetch

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-
 cd /home/bitnami/inzight-www
 /opt/bitnami/git/bin/git fetch
+
+TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 /opt/bitnami/git/bin/git checkout $TAG
