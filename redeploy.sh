2fe3c730ef21d98ae591d6947894b8d96e2650b3
Tom Elliott <tomelliottnz@gmail.com>
2020-09-14 16:01:24 +1200

    No need for origin

--- redeploy.sh
+++ redeploy.sh
@@ -4,4 +4,4 @@ TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 
 cd /home/bitnami/inzight-www
 /opt/bitnami/git/bin/git fetch
-/opt/bitnami/git/bin/git checkout origin $TAG
+/opt/bitnami/git/bin/git checkout $TAG
