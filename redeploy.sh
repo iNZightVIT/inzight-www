bd7035d166c9d64a2e2010d7a739d277cd850a3f
Tom Elliott <tomelliottnz@gmail.com>
2020-09-14 16:18:39 +1200

    Update

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 cd /home/bitnami/inzight-www
-/opt/bitnami/git/bin/git fetch
-
-TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-/opt/bitnami/git/bin/git checkout $TAG
+/opt/bitnami/git/bin/git pull origin master
 
 # any changes to webhook.service
 cp webhook.service /lib/systemd/system/
