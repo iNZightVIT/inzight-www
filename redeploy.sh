727059c3c1095c800fcbe5a680af6d740bc3a576
Tom Elliott <tomelliottnz@gmail.com>
2020-09-14 16:16:04 +1200

    Update redeloy

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-# any changes to webhook.service
-#
-
 cd /home/bitnami/inzight-www
 /opt/bitnami/git/bin/git fetch
 
 TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 /opt/bitnami/git/bin/git checkout $TAG
+
+# any changes to webhook.service
+cp inzight-www/webhook.service /lib/systemd/system/
+cp /lib/systemd/system/webhook.service /etc/systemd/system/webhook.service
+chmod 644 /etc/systemd/system/webhook.service
+systemctl daemon-reload
+systemctl restart webhook.service
