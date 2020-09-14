fef20f0004e99d260ff64e2da9d012d61de4e03b
Tom Elliott <tomelliottnz@gmail.com>
2020-09-15 10:09:32 +1200

    get inzight package info from repo

--- website_version.R
+++ website_version.R
@@ -3,11 +3,22 @@ s <- readLines(f)
 lv <- grep("$inzight_version", s, fixed = TRUE)
 ld <- grep("$release_date", s, fixed = TRUE)
 
-DESC <- packageDescription("iNZight")
+# get DESCRIPTION file
+r <- "https://r.docker.stat.auckland.ac.nz"
+V <- available.packages(repos = r)["iNZight", "Version"]
+url <- sprintf("%s/src/contrib/iNZight_%s.tar.gz", r, V)
+t <- tempfile()
+on.exit(unlink(t))
+dir.create("pkg")
+on.exit(unlink("pkg", TRUE, TRUE), add = TRUE)
+download.file(url, t)
+untar(t, exdir = "pkg")
 
-s[lv] <- sprintf(gsub("\".+\"", "\"%s\"", s[lv]), DESC$Version)
+DESC <- read.dcf("pkg/iNZight/DESCRIPTION")[1,]
+
+s[lv] <- sprintf(gsub("\".+\"", "\"%s\"", s[lv]), DESC["Version"])
 s[ld] <- sprintf(gsub("\".+\"", "\"%s\"", s[ld]),
-    format(as.Date(DESC$Date), "%e %B %Y")
+    format(as.Date(DESC["Date"]), "%e %B %Y")
 )
 
 writeLines(s, f)
