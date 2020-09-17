02a606e6f0d1ea4884a98e996c5b7be189c60570
Tom Elliott <tomelliottnz@gmail.com>
2020-09-18 09:12:34 +1200

    Add favicons and theme colours

--- iNZight/assets/includes/1-top_matter.php
+++ iNZight/assets/includes/1-top_matter.php
@@ -36,6 +36,7 @@ $VLITE = $_SESSION["VLITE"];
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#094b85">
 
     <?php
       // Allow us to define extra meta tags:
@@ -49,9 +50,14 @@ $VLITE = $_SESSION["VLITE"];
       if (preg_match('/inzight.nz/', $_SERVER['HTTP_HOST']) === 1) {
         include_once($rel . 'assets/includes/analyticstracking.php');
       } else {
-        echo "<!-- development environment here .. -->";
+        echo "<!-- development environment -->";
       }
     ?>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     <link rel="stylesheet" type="text/css" href="<?php echo $rel; ?>css/main.css">
 
