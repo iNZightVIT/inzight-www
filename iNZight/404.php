71bdd5380f7691614907bb0f869d203d0fcc7f4a
Tom Elliott <tomelliottnz@gmail.com>
2020-09-22 08:52:09 +1200

    Add 404 page (closes #23)

---
+++ iNZight/404.php
@@ -0,0 +1,22 @@
+<?php
+$rel = "./";
+require_once('assets/objects/setup.php');
+
+require_once('assets/includes/1-top_matter.php');
+require_once('assets/includes/2-header.php');
+
+?>
+
+<div class="container">
+  <h2>404: Page not found</h2>
+  <h3>Uh oh ... the page you're looking for doesn't seem to exist ...</h3>
+
+  <p>
+    If you typed the URL, check you entered it correctly.
+    If you copied the link from some documentation somewhere, or used a link on the website, please let us know that the page no longer seems to exist (or has been moved) so we can fix it! <a href="mailto:inzight_support@stat.auckland.ac.nz">inzight_support@stat.auckland.ac.nz</a>.
+  </p>
+</div>
+
+<?php
+require_once('assets/includes/3-bottom_matter.php');
+?>
