438e6d6b24c6d991a12930c44e400a5e77b364fa
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-02-05 09:28:42 +1300

    include md funs

--- iNZight/install/windows.php
+++ iNZight/install/windows.php
@@ -37,9 +37,10 @@ require_once($rel . 'assets/functions/filesize.php');
       <summary>What's new in the developmental version?</summary>
       <?php
         if (file_exists($rel . 'install/nightly_changes.md')) {
-          // $Pd = new ParsedownExtra();
-          // $text = file_get_contents($rel . "install/nightly_changes.md");
-          // echo $Pd->text($text);
+          include_once($rel . 'assets/libraries/md.php');
+          $Pd = new ParsedownExtra();
+          $text = file_get_contents($rel . "install/nightly_changes.md");
+          echo $Pd->text($text);
         }
       ?>
     </details>
