509464f6b39dc46468beaca95bc63a3c0c111340
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-03-03 12:42:21 +1300

    add fb message link

--- iNZight/assets/includes/1-top_matter.php
+++ iNZight/assets/includes/1-top_matter.php
@@ -90,6 +90,34 @@ $VLITE = $_SESSION["VLITE"];
   </head>
 
   <body>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="100566192069494"
+        theme_color="#18afe3"
+        logged_in_greeting="Kia ora, how can we help?"
+        logged_out_greeting="Kia ora, how can we help?">
+      </div>
+
     <?php
       if (!isset($_SESSION["hide"])) {
         $_SESSION["hide"] = false;
