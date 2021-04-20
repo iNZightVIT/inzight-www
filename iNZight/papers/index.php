ec85f2789ba4375b0d77e4e17f1c6e0d23a806b0
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-04-20 14:32:02 +1200

    add papers page

---
+++ iNZight/papers/index.php
@@ -0,0 +1,41 @@
+<?php
+$rel = "../";
+require_once($rel . 'assets/includes/1-top_matter.php');
+require_once($rel . 'assets/includes/2-header.php');
+
+$papers = array(
+    array(
+        'title' => "iNZight: A Graphical User Interface for Data Visualisation and Analysis through R",
+        'authors' => "Elliott, Wild, Barnett, & Sporle",
+        'year' => "2021",
+        'journal' => "Journal of Statistical Software",
+        'url' => "https://github.com/iNZightVIT/papers/raw/main/2021_jss/index.pdf"
+    )
+);
+
+?>
+
+<h3>Journal Articles</h3>
+
+
+<div class="contents_list">
+  <div class="label">Table of Contents</div>
+
+  <ol>
+    <?php
+      foreach ($papers as $paper) {
+        echo "<li>";
+        echo "<a href='$paper->url'>$paper->authors ($paper->year). $paper->title. $paper->journal.</a>";
+        echo "</li>";
+      }
+    ?>
+  </ol>
+</div>
+
+
+
+
+
+<?php
+require_once($rel . 'assets/includes/3-bottom_matter.php');
+?>
