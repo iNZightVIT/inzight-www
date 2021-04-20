d06d1cf8382c666653b5ca9210dbcaf8756ec8ad
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-04-20 15:19:50 +1200

    add authors + link

--- iNZight/papers/index.php
+++ iNZight/papers/index.php
@@ -24,6 +24,11 @@ $papers = array(
     ?>
 
     <h3><?php echo $paper['title']; ?></h3>
+    <h5><?php echo $paper['authors']; ?></h5>
+
+    <p>
+        URL: <a href="<?php echo $paper['url']; ?>"><?php echo $paper['url']; ?></a>
+    </p>
 
     <?php } else { ?>
 
