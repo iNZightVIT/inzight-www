fd8214bb97367d2c63b0fb109feb5c0b3f5bd869
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-04-21 08:32:01 +1200

    structure

--- iNZight/papers/2021_jss.pdf.php
+++ iNZight/papers/2021_jss.pdf.php
@@ -1,1 +1,15 @@
-hello
+<?php
+$rel = "../";
+require_once($rel . 'assets/includes/1-top_matter.php');
+require_once($rel . 'assets/includes/2-header.php');
+
+?>
+
+<div class="container">
+    <h1>This is a paper</h1>
+
+</div>
+
+<?php
+require_once($rel . 'assets/includes/3-bottom_matter.php');
+?>
