a3b6f8418e99bd7614c1e70c2bfb538cc4221629
Tom Elliott <tomelliottnz@gmail.com>
2021-04-21 13:30:50 +1200

    hmm..

--- iNZight/releases/4.1/index.php
+++ iNZight/releases/4.1/index.php
@@ -2,15 +2,15 @@
 $rel = "../";
 require_once($rel . 'assets/includes/1-top_matter.php');
 require_once($rel . 'assets/includes/2-header.php');
-
 include_once($rel . 'assets/libraries/md.php');
+
 ?>
 
 <div class="container">
 
   <?php
-    $Pd = new ParsedownExtra();
-    $text = file_get_contents("info.Md");
+    // $Pd = new ParsedownExtra();
+    // $text = file_get_contents("info.Md");
     // echo $Pd->text($text);
   ?>
 
