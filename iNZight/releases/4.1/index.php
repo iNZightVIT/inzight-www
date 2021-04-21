3a27ef0d71204d5f457c5b21f44141f010f7b664
Tom Elliott <tomelliottnz@gmail.com>
2021-04-21 13:33:32 +1200

    more comments...

--- iNZight/releases/4.1/index.php
+++ iNZight/releases/4.1/index.php
@@ -2,12 +2,12 @@
 $rel = "../";
 require_once($rel . 'assets/includes/1-top_matter.php');
 require_once($rel . 'assets/includes/2-header.php');
-include_once($rel . 'assets/libraries/md.php');
+// include_once($rel . 'assets/libraries/md.php');
 
 ?>
 
 <div class="container">
-
+  <h3>Hello</h3>
 
 </div>
 
