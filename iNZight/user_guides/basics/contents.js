fbf2940cc7d2028bbc4ead34af37515528b92d7c
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-04-23 07:36:48 +1200

    add outer [] to contents.js

--- iNZight/user_guides/basics/contents.js
+++ iNZight/user_guides/basics/contents.js
@@ -1,35 +1,37 @@
-{
-"index" : {
-  "title" : "The Basics",
-  "desc"  : "This section will outline the very basics of iNZight. Thanks to the simplicity of the design, you will be exploring your data within minutes!"
-},
-"getting_started.php" : {
-  "title" : "Getting started with iNZight",
-  "short" : "Getting Started",
-  "desc"  : "Start up iNZight for the first time."
-},
-"load_data" : {
-  "title" : "Load data into iNZight",
-  "short" : "Load Data",
-  "desc"  : "From starting up iNZight and reading in a data file, to your first graphs."
-},
-"plot1var"  : {
-  "title" : "Plotting one variable",
-  "short" : "One Variable",
-  "desc"  : "Working with one variable at a time - the 'Variable 1' box."
-},
-"plot2var"  : {
-  "title" : "Plotting two variables",
-  "short" : "Two Variables",
-  "desc"  : "Working with pairs of variables - the 'Variable 2' box."
-},
-"subset_data"  : {
-  "title" : "Subsetting your data",
-  "short" : "Subsetting",
-  "desc"  : "Multiple plots for subsets of the data - the 'Subset Variable 1' and 'Subset variable 2' boxes."
-},
-"update_inzight" : {
-  "title" : "Updating iNZight",
-  "desc"  : "Keep up-to-date with the newest features, changes, and bug-fixes by updating your copy of iNZight."
-}
-}
+[
+  {
+    "index" : {
+      "title" : "The Basics",
+      "desc"  : "This section will outline the very basics of iNZight. Thanks to the simplicity of the design, you will be exploring your data within minutes!"
+    },
+    "getting_started.php" : {
+      "title" : "Getting started with iNZight",
+      "short" : "Getting Started",
+      "desc"  : "Start up iNZight for the first time."
+    },
+    "load_data" : {
+      "title" : "Load data into iNZight",
+      "short" : "Load Data",
+      "desc"  : "From starting up iNZight and reading in a data file, to your first graphs."
+    },
+    "plot1var"  : {
+      "title" : "Plotting one variable",
+      "short" : "One Variable",
+      "desc"  : "Working with one variable at a time - the 'Variable 1' box."
+    },
+    "plot2var"  : {
+      "title" : "Plotting two variables",
+      "short" : "Two Variables",
+      "desc"  : "Working with pairs of variables - the 'Variable 2' box."
+    },
+    "subset_data"  : {
+      "title" : "Subsetting your data",
+      "short" : "Subsetting",
+      "desc"  : "Multiple plots for subsets of the data - the 'Subset Variable 1' and 'Subset variable 2' boxes."
+    },
+    "update_inzight" : {
+      "title" : "Updating iNZight",
+      "desc"  : "Keep up-to-date with the newest features, changes, and bug-fixes by updating your copy of iNZight."
+    }
+  }
+]
