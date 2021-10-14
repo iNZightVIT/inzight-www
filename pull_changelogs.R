332a2faa81b410b86880205c7c457af3134198ec
Tom Elliott <tomelliottnz@gmail.com>
2021-10-15 10:17:58 +1300

    use extension Md

--- pull_changelogs.R
+++ pull_changelogs.R
@@ -13,6 +13,6 @@ pkgs <- c(
 for (pkg in pkgs) {
     # download NEWS.md -> iNZight/support/changelog/changes/{pkg}.Md
     url <- sprintf("https://raw.githubusercontent.com/iNZightVIT/%s/master/NEWS.Md", pkg)
-    file <- sprintf("iNZight/support/changelog/changes/%s.md", pkg)
+    file <- sprintf("iNZight/support/changelog/changes/%s.Md", pkg)
     download.file(url, file, quiet = TRUE)
 }
