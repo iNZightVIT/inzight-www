5185eed7f6dc325e87475a030675f32f6e57badf
Tom Elliott <tomelliottnz@gmail.com>
2021-12-14 09:38:33 +1300

    Start using md instead of Md

--- nightly_version.R
+++ nightly_version.R
@@ -17,7 +17,8 @@ s[ld] <- sprintf(gsub("\".+\"", "\"%s\"", s[ld]),
 writeLines(s, f)
 
 # and grab DESCRIPTION file for DEV build
-url <- "https://raw.githubusercontent.com/iNZightVIT/iNZight/dev/NEWS.Md"
+url <- "https://raw.githubusercontent.com/iNZightVIT/iNZight/dev/NEWS.md"
+if (!RCurl::url.exists(url)) url <- gsub("md$", "Md", url)
 changes <- readLines(url)
 changes <- changes[1:(min(grep("^#", changes)) - 1L)]
 
--- pull_changelogs.R
+++ pull_changelogs.R
@@ -11,8 +11,9 @@ pkgs <- c(
 )
 
 for (pkg in pkgs) {
-    # download NEWS.md -> iNZight/support/changelog/changes/{pkg}.Md
-    url <- sprintf("https://raw.githubusercontent.com/iNZightVIT/%s/master/NEWS.Md", pkg)
-    file <- sprintf("iNZight/support/changelog/changes/%s.Md", pkg)
+    # download NEWS.md -> iNZight/support/changelog/changes/{pkg}.md
+    url <- sprintf("https://raw.githubusercontent.com/iNZightVIT/%s/master/NEWS.md", pkg)
+    if (!RCurl::url.exists(url)) url <- gsub("md$", "Md", url)
+    file <- sprintf("iNZight/support/changelog/changes/%s.md", pkg)
     download.file(url, file, quiet = TRUE)
 }
