73ca4101b1be011039b15fe528b58f30d658d51a
Tom Elliott <tomelliottnz@gmail.com>
2022-12-02 14:55:03 +1300

    fixes

--- iNZight/support/contact/submit.php
+++ iNZight/support/contact/submit.php
@@ -13,6 +13,7 @@ use MailerSend\Helpers\Builder\Personalization;
 use MailerSend\Helpers\Builder\Attachment;
 use MailerSend\Helpers\Builder\Recipient;
 use MailerSend\Helpers\Builder\EmailParams;
+use MailerSend\Exceptions\MailerSendValidationException;
 
 // some filters
 function clean_num($a)
@@ -194,8 +195,6 @@ $ticketParams = (new EmailParams())
   ->setVariables($ticketVars)
   ->setPersonalization($ticketDetails);
 
-$mailersend->email->send($ticketParams);
-
 // read reply.template file
 $reply = file_get_contents(__DIR__ . '/reply.template');
 $replyTxt = file_get_contents(__DIR__ . '/reply_text.template');
@@ -210,154 +209,15 @@ $emailParams = (new EmailParams())
     ->setText($replyTxt)
     ->setVariables($variables);
 
-$mailersend->email->send($emailParams);
-
-
-die('OK');
-
-$boundary = "==Multipart_Boundary_x" . md5(date('r', time())) . "x";
-
-$attachment = '';
-
-if ($p['screenshot']['error'] === 0) {
-  $f_data = chunk_split(base64_encode(file_get_contents($p['screenshot']['tmp_name'])));
-  $f_name = $p['screenshot']['name'];
-  $attachment .= "--{$boundary}\n" .
-    "Content-Type: application/octet-stream; name=\"$f_name\"\n" .
-    "Content-Disposition: attachment;\n" .
-    "Content-Transfer-Encoding: base64\n\n" . $f_data . "\n\n";
+$sendError = false;
+try {
+  $mailersend->email->send($ticketParams);
+  $mailersend->email->send($emailParams);
+} catch (MailerSendValidationException $e) {
+  $sencError = true;
 }
 
-if ($p['dataset']['error'] === 0) {
-  $f_data = chunk_split(base64_encode(file_get_contents($p['dataset']['tmp_name'])));
-  $f_name = $p['dataset']['name'];
-  $attachment .= "--{$boundary}\n" .
-    "Content-Type: application/octet-stream; name=\"$f_name\"\n" .
-    "Content-Disposition: attachment;\n" .
-    "Content-Transfer-Encoding: base64\n\n" . $f_data . "\n\n";
-}
-
-$headers  = "MIME-Version: 1.0" . "\r\n";
-$headers .=
-  (isset($attachment) ? "Content-type:multipart/mixed;\n boundary=\"$boundary\";" : "Content-type:text/html;") .
-  "\r\n";
-if (strlen($email) > 0) {
-  $headers_conf =
-    "From: $sendto \r\n".
-    "MIME-Version: 1.0" . "\r\n" .
-    "Content-type: text/html; charset=UTF-8" . "\r\n";
-
-
-  "MIME-Version: 1.0" . "\r\n" .
-      "Content-type:text/html;\r\n" .
-      "From: " . $sendto . "\r\n";
-
-  $rts = "";
-  if (strlen($name) > 0) {
-    $rts .= $name . " <" . $email . ">";
-  } else {
-    $rts .= $email;
-  }
-  if ($p["inzight_version"] == "lite") {
-    $rts .= ", iNZight Lite Support <" . $sendto . ">";
-  } else {
-    $rts .= ", iNZight Support <" . $sendto . ">";
-  }
-  $headers .= "Reply-to: " . $rts . "\r\n";
-}
-$headers .= "From: " . $sendto . "\r\n";
-
-$message = "";
-$message .= "<div style ='padding: 1em 0.5em'>";
-$message .= nl2br($p["message_content"]);
-$message .= "</div>";
-
-
-$message .= "<div style='font-size: 0.8em; background: #ccc; padding: 0.5em; line-height: 2em'>";
-$message .= "<b>Technical Information</b><br>";
-$message .= "Sent on " . date("l jS \of F Y, h:i:s A") . "<br>";
-if (strlen($name) > 0) {
-  $message .= "Name: " . $name . "<br>";
-}
-if (strlen($email) > 0) {
-  $message .= "Reply to: " . $email . "<br>";
-} else {
-  $message .= "No reply email supplied.<br>";
-}
-
-if ($inz == "lite") {
-  $message .= "iNZight Lite accessed from <b>" . $ver . "</b> using <b>" . $inzdet . "</b><br>";
-} else {
-  $message .= "Installation Info: " . $os . ", iNZight " . $ver . "<br>";
-}
-
-if (strlen($class_info) > 0) {
-  $message .= "Class information: <em>" . $class_info . "</em><br/>";
-}
-
-$s = $_SERVER;
-
-// $message .= "<br><b>Debugging Info</b><br>";
-// $message .= "User Agent: " . $s["HTTP_USER_AGENT"] . "<br>";
-// $message .= "HTTP Language: " . $s["HTTP_ACCEPT_LANGUAGE"] . "<br>";
-
-
-// Additional log file info
-// if (isset($p['log_file']['content'])) {
-//   $message .= "<br><b>iNZight Log File</b></br>";
-//   $message .= $p['log_file']['content'];
-// }
-
-
-$message .= "</div>";
-$message_conf = $message;
-$message = "<div style='font-family: sans-serif; width: 100%; font-size: 14px'>" .
-  $message . "</div>";
-
-if (strlen($email) > 0) {
-  $hello = "Kia ora";
-  if (strlen($name) > 0) $hello .= " " . $name;
-  $message_conf = "<div style='font-family: sans-serif; width: 100%; font-size: 14px'>" .
-    $hello . ",<br><br>" .
-    "We have recieved your message and will be in touch as soon as possible. " .
-    "Please reply to this email if you have any follow-up queries or additional information." .
-    "<br><br>Thanks,<br>The iNZight Team." .
-    "<br><hr>" .
-    $message_conf . "</div>";
-}
-
-$multimessage = '';
-if (isset($attachment)) {
-  $multimessage .= "--$boundary\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
-    "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n" .
-    $attachment . "\n" .
-    "--$boundary}--";
-
-  $message = $multimessage;
-}
-
-// $res = mail($email, $subject, $message_conf, $headers_conf);
-// if ($res) {
-//   echo "success - ";
-// } else {
-//   echo "fail - ";
-// }
-
-if (mail($sendto, $subject, $message, $headers)) {
-  if (strlen($email) > 0) {
-    mail($email, $subject, $message_conf, $headers_conf);
-    // echo "Email: ";
-    // print_r($email);
-    // echo "<br>Subject: ";
-    // print_r($subject);
-    // echo "<br>Message: <br><br>";
-    // print_r($message_conf);
-    // echo "<br><br>Headers: <br><pre>";
-    // print_r($headers_conf);
-    // echo "</pre>";
-
-    // die($res);
-  }
+if (!$sendError) {
   header("Location: success.php");
   die("Message sent. Thank you.");
 }
