1bc19f5805584645e4c25636dc9f5d8575f2650f
Tom Elliott <tomelliottnz@gmail.com>
2023-07-24 10:01:29 +1200

    add matomo tracking url

--- iNZight/assets/includes/1-top_matter.php
+++ iNZight/assets/includes/1-top_matter.php
@@ -114,6 +114,22 @@ $VLITE = $_SESSION["VLITE"];
     <script src="<?php echo $rel;?>js/bootstrap.min.js"></script>
 
     <title>iNZight for Data Analysis</title>
+
+    <!-- Matomo -->
+    <script>
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//analytics-production.up.railway.app/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '3']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Matomo Code -->
   </head>
 
   <body>
